e66f5333a0eb66a1b380ed6a16538c02f0a2ccc4
Aymane <aymanechaaba@gmail.com>
2021-10-30 16:00:16 +0100

    Changing the action attribute

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
-
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from "firebase/database";
-
-// Set the configuration for your app
-// TODO: Replace with your project's config object
-const firebaseConfig = {
-  apiKey: "AIzaSyBOQJHkKaX2o_O5JsxvftSAeogIK2S0EzE",
-  authDomain: "buzzmedia-english-form.firebaseapp.com",
-  databaseURL: "https://buzzmedia-english-form-default-rtdb.firebaseio.com",
-  projectId: "buzzmedia-english-form",
-  storageBucket: "buzzmedia-english-form.appspot.com",
-  messagingSenderId: "111626673022",
-  appId: "1:111626673022:web:488a634940b4c5aabeb12f",
-  measurementId: "G-79WKSLCDEY"
-};
-
-const app = initializeApp(firebaseConfig);
-
-// Get a reference to the database service
-const database = getDatabase(app);
+import './node_modules/bootstrap-icons/'
 
 var form = document.querySelector(".main-form");
 form.addEventListener("submit", function (event) {
@@ -31,33 +11,14 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
   var firstName = getInputVal("firstname");
   var lastName = getInputVal("lastname");
+  var help = getInputVal("");
   var email = getInputVal("email");
+  var 
 
-  //Save message
-  saveMessage(
-    firstName,
-    lastName,
-    email
-  );
+  console.log(firstName)
 });
 
-var messagesRef = app.firebase.database().ref("messages");
 
 function getInputVal(id) {
-  return document.getElementById(id).value;
-}
-
-//Save message to firebase
-function saveMessage(
-  firstName,
-  lastName,
-  email
-) {
-  var newMessageRef = messagesRef.push();
-
-  newMessageRef.set({
-    firstName: firstName,
-    lastName: lastName,
-    email: email
-  });
+  document.getElementById(id);
 }
