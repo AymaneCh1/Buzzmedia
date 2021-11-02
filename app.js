b537554cbca0cc338762a8466b459c6315535b3f
Aymane <aymanechaaba@gmail.com>
2021-11-02 22:27:24 +0100

    added ecommerce page

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-import './node_modules/bootstrap-icons/'
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
 
-var form = document.querySelector(".main-form");
-form.addEventListener("submit", function (event) {
-  // if(form.checkValidity() === false) {
-  //     event.preventDefault();
-  //     event.stopPropagation();
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
 
-  //     form.classList.add('was-validated');
-  // }
-  event.preventDefault();
-  var firstName = getInputVal("firstname");
-  var lastName = getInputVal("lastname");
-  var help = getInputVal("");
-  var email = getInputVal("email");
-  var 
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
 
-  console.log(firstName)
-});
-
-
-function getInputVal(id) {
-  document.getElementById(id);
-}
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
